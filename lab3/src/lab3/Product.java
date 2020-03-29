bd255db229da4ea035f151dc5f47740640e6affe
psliwinski <49346247+psliwinski@users.noreply.github.com>
2020-03-29 17:20:53 +0200

    Add files via upload

---
+++ lab3/src/lab3/Product.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Product {
+    private Integer id; 
+    private String name; 
+    private String category;
+    private Double price; 
+    
+    public Product(Integer id, String name, String category, Double price) {
+
+    }
+}
